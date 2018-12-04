13a4479c8f58029322152c49ef0683cb254d328d
Hussein Al Hammad <husseinalhammad@gmail.com>
2018-12-04 15:44:14 +0400

    Update view button

--- pipit_catalog/assets/js/product_preview.js
+++ pipit_catalog/assets/js/product_preview.js
@@ -8,17 +8,15 @@ if(pageUrl.includes('apps/perch_shop_products/product/edit')) {
 
     // add button
     var smartbar = document.querySelector('.smartbar ul');
-    var previewUrl = '/perch/addons/apps/pipit_catalog/product_view.php?id='+id;
+    var previewUrl = Perch.path+'/addons/apps/pipit_catalog/product_view.php?id='+id;
 
     let html = `<li class="smartbar-end smartbar-util">
-        <a href="${previewUrl}" title="View" class="viewext viewext" target="_blank" rel="noopener">
-            <svg role="img" width="14" height="14" class="icon icon-document" title="View" aria-label="Preview Draft"> <use xlink:href="/perch/core/assets/svg/core.svg#document"></use></svg> 
+        <a href="${previewUrl}" title="View" class="viewext" target="_blank" rel="noopener">
+            <svg role="img" width="14" height="14" class="icon icon-o-world" title="View Page" aria-label="View Page"> <use xlink:href="/perch/core/assets/svg/core.svg#o-world"></use> </svg>
             <span>View</span>
         </a>
     </li>`
 
     smartbar.insertAdjacentHTML('beforeend', html);
 
-} else {
-    
 }
