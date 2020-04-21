6be7d7820e3873a660e9cc1db3de1ae8aaec7a55
Hussein Al Hammad <husseinalhammad@gmail.com>
2020-04-21 15:25:36 +0400

    reorder and republish privileges

--- pipit_catalog/modes/update.pre.php
+++ pipit_catalog/modes/update.pre.php
@@ -3,5 +3,7 @@
 $API = new PerchAPI(1.0, 'pipit_catalog');
 $UserPrivileges = $API->get('UserPrivileges');
 $UserPrivileges->create_privilege('pipit_catalog', 'Access the Catalog app');
+$UserPrivileges->create_privilege('pipit_catalog.reorder', 'Reorder products');
+$UserPrivileges->create_privilege('pipit_catalog.republish', 'Republish products');
 
 $Settings->set('pipit_catalog_update', PIPIT_CATALOG_VERSION);
--- pipit_catalog/update/activate.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-    $API = new PerchAPI(1.0, 'pipit_catalog');
-    $UserPrivileges = $API->get('UserPrivileges');
-    $UserPrivileges->create_privilege('pipit_catalog', 'Access the Catalog app');
