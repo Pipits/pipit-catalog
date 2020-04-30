2a549667ff72f7f5e8df09f05ceacc429c034f3d
Hussein Al Hammad <husseinalhammad@gmail.com>
2020-04-30 15:24:15 +0400

    pipit_catalog_get_variants()

--- pipit_catalog/runtime.php
+++ pipit_catalog/runtime.php
@@ -18,3 +18,87 @@
         if( $return || (isset($opts['skip-template']) && $opts['skip-template']) ) return $products;
         echo $products['html'];
     }
+
+
+
+
+
+
+
+
+    /**
+     * @param string $slug
+     * @return array
+     */
+    function pipit_catalog_get_variants($slug) {
+        $products = perch_shop_product_variants($slug, [
+            'skip-template' => true,
+            'api' => true,
+        ]);
+    
+    
+        $keys = [
+            'productID',
+            'parentID',
+            'slug',
+            'sku',
+            'productVariantDesc',
+    
+            'price',
+            'sale_price',	
+            'trade_price',	
+            'on_sale',	
+            
+            
+            'stock_status',
+            'stock_level',
+            'stock_location',
+            
+            // 'has_variants',
+        ];
+    
+    
+    
+        $opt_keys = [
+            'optionID',
+            'optionTitle',
+            'options',
+            // 'productvalues',
+        ];
+
+
+        $options = [];
+
+        if(isset($products[0]['options'])) {
+            $options = $products[0]['options'];
+        }
+
+        $options = array_map(function(&$item) use($opt_keys) {
+            $out = [];
+    
+            foreach($opt_keys as $key) {
+                $out[$key] = (isset($item[$key])) ? $item[$key] : false;
+            }
+            
+            return $out;
+        }, $options);
+    
+
+
+
+        $products = array_map(function(&$item) use($keys) {
+            $out = [];
+    
+            foreach($keys as $key) {
+                $out[$key] = (isset($item[$key])) ? $item[$key] : false;
+            }
+            
+            return $out;
+        }, $products);
+
+
+
+
+    
+        return ['products' => $products, 'options' => $options];
+    }
